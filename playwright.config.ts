cc4b6ce73f305972fd8a989d486c5413917f1d09
Melissa Goulart <melissa.goulart@dbserver.com.br>
2026-08-01 11:30:53 -0300

    chore: configura projeto Playwright com TypeScript

---
+++ playwright.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+
+  projects: [
+    {
+      name: 'web',
+      testDir: './tests/web',
+      use: {
+        baseURL: 'https://demo.playwright.dev/todomvc/#/',
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'api',
+      testDir: './tests/api',
+      use: {
+        baseURL: 'https://api.github.com',
+      },
+    },
+  ],
+});
